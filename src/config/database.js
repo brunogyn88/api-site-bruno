079cf789579521eea1e7a306c2ebb94704f6e165
Bruno Alves <brunogyn.eng@gmail.com>
2020-03-12 14:59:34 -0300

    configuração de conexão com o banco de dados e criação do banco de dados

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'postgres',
+  database: 'basebd',
+  define: {
+    timestamp: true,
+    underscored: true
+  }
+};
